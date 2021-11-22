5fe9a8138bcde7feda5131e8029309d34e8f9dc6
cristianlemeni <37481559+CristianLemeni@users.noreply.github.com>
2021-11-22 09:19:58 +0200

    Update reels.ts

--- src/webgl/reels.ts
+++ src/webgl/reels.ts
@@ -1,7 +1,7 @@
 import * as gsap from 'gsap'
 import * as PIXI from 'pixi.js'
 import { Init } from './init'
-import { MotionBlurFilter } from '@pixi/filter-motion-blur';
+import {MotionBlurFilter} from '@pixi/filter-motion-blur';
 import { Howl } from 'howler';
 
 
@@ -16,25 +16,24 @@ export class Reels {
     idsDict: any
     sounds: Array<Howl>
 
-    constructor(stage: PIXI.Container, sounds: Array<Howl>) {
+    constructor(stage: PIXI.Container, sounds: Array<Howl>){
         this.stage = stage
         this.reelsContainer = new PIXI.Container()
 
         this.stage.addChild(this.reelsContainer)
 
-        this.spinTimeline = new gsap.TimelineMax({ paused: true, duration: 3 })
-        this.spinTimeline.eventCallback("onComplete", this.checkForWin, [this]);
+        this.spinTimeline = new gsap.TimelineMax({paused: true, duration: 3})
+        this.spinTimeline.eventCallback("onComplete", this.checkForWin,  [this]);
         this.addEvents()
 
         this.reelsJson = require('/public/assets/reels.json').reels;
 
         this.ids = []
         this.idsDict = {}
-
         this.sounds = sounds
     }
 
-    initReels(reelsTextures: any, spinBtnTexture: any, skipBtnTexture: any) {
+    initReels(reelsTextures: any, spinBtnTexture: any, skipBtnTexture: any){
         this.addReels(reelsTextures)
         this.addSpinButton(spinBtnTexture)
         this.addSkipButton(skipBtnTexture)
@@ -45,84 +44,81 @@ export class Reels {
         this.reelsContainer.setChildIndex(mask, this.reelsContainer.children.length - 3)
     }
 
-    addReels(textures: any) {
+    addReels(textures: any){
         let symbWidth = 0
         let y = 0
         let x = 0
-        let soundInd = 0
-       
-        for(let i = 0; i < 3; i++){
-            x = 0
-            let reelRow = new PIXI.Container()
+        for(let i = 0; i < 5; i++){
+            y = 0
+            let soundInd = 0
+            let reelCol = new PIXI.Container()
             this.ids.push([])
-            for(let j = 0; j < 5; j++){
+            for(let j = 0; j < 3; j++){
                 let reelSymbol = new PIXI.Sprite(textures["icon" + this.getIconId(this.reelsJson[i].reel.icons) + ".png"]);
 
                 reelSymbol.position.set(x, y)
-                reelRow.addChild(reelSymbol)
-
-                this.reelsContainer.addChild(reelRow)
-                x += reelSymbol.width * 1.15
+                reelCol.addChild(reelSymbol)
+            
+                
+                this.reelsContainer.addChild(reelCol)
+                y += reelSymbol.height * 1.15 
                 symbWidth = reelSymbol.width
             }
             this.addID.bind(this)
             this.spinTimeline.add(() => {
                 //@ts-ignore
-                reelRow.filters = [new MotionBlurFilter([1, 2], 9)]
+                reelCol.filters = [new MotionBlurFilter([1,2], 9)] 
             }, 0)
-            this.spinTimeline.fromTo(reelRow.position, 0.5, { y: reelRow.y }, { y: reelRow.height * 1.25, ease: "back.inOut(1.7)" }, i * 0.15)
+            this.spinTimeline.fromTo(reelCol.position, 0.5, {y: reelCol.y}, {y: reelCol.height * 1.25, ease: "back.inOut(1.7)"}, i*0.15)
             this.spinTimeline.add(() => {
-                reelRow.position.y = - reelRow.height * 1.25
-                for (let r = 0; r < reelRow.children.length; r++) {
+                reelCol.position.y = - reelCol.height * 1.25
+                for(let r = 0; r < reelCol.children.length; r++){
                     let id = this.getIconId(this.reelsJson[i].reel.icons)
                     this.addID(id, i);
-                    (reelRow.children[r] as PIXI.Sprite).texture = textures["icon" + id + ".png"]
+                    (reelCol.children[r] as PIXI.Sprite).texture =  textures["icon" + id + ".png"]
                 }
             })
-            this.spinTimeline.to(reelRow.position, 0.5, { y: 0, ease: "back.inOut(1.7)" })
+            this.spinTimeline.to(reelCol.position, 0.5, {y: 0, ease: "back.inOut(1.7)"})
             this.spinTimeline.add(() => {
-                reelRow.filters = []
-                soundInd++
-                if(soundInd >= 5){
-                    soundInd = 0 
-                }
+                reelCol.filters = [] 
                 this.sounds[soundInd].play()
             })
-            reelRow.position.x = 0
-            y += symbWidth! * 1.15
+            soundInd++
+            reelCol.position.y = 0
+
+            x += symbWidth! * 1.15
         }
 
-        this.resize()
+       this.resize()
     }
 
-    addSpinButton(texture: any) {
+    addSpinButton(texture: any){
         let spinButton = new PIXI.Sprite(texture);
         spinButton.interactive = true
         spinButton.scale.set(0.5)
         spinButton.position.x = this.reelsContainer.children[0].position.x - spinButton.width * 1.5
-        spinButton.position.y = this.reelsContainer.children[0].position.y + spinButton.height / 6
+        spinButton.position.y = this.reelsContainer.children[0].position.y + spinButton.height/6
         this.reelsContainer.addChild(spinButton)
         this.resize()
 
-        spinButton.on("pointerdown", (e: any) => {
+        spinButton.on("mousedown", (e: any) => {
             let evt = new Event("spinPressed")
             document.dispatchEvent(evt)
             this.sounds[0].play()
         });
-        
     }
 
-    addSkipButton(texture: any) {
+    addSkipButton(texture: any){
         let skipButton = new PIXI.Sprite(texture);
         skipButton.interactive = true
         skipButton.scale.set(0.5)
         skipButton.position.x = this.reelsContainer.children[0].position.x - skipButton.width * 1.5
-        skipButton.position.y = this.reelsContainer.children[0].position.y + skipButton.height / 6
+        skipButton.position.y = this.reelsContainer.children[0].position.y + skipButton.height/6
         this.reelsContainer.addChild(skipButton)
         skipButton.visible = false
         this.resize()
 
-        skipButton.on("mousedown", function (e: any) {
+        skipButton.on("mousedown", function(e: any) {
             let evt = new Event("skipPressed")
             document.dispatchEvent(evt)
         });
@@ -130,17 +126,17 @@ export class Reels {
 
     getIconId(arr: any) {
         let parsedArr = arr.split(",")
-        let randInt = Math.floor(Math.random() * ((parsedArr.length - 1) + 1));
+        let randInt = Math.floor(Math.random() * ((parsedArr.length-1) + 1));
         return parsedArr[randInt].toString()
     }
 
     resize() {
         this.reelsContainer.scale.set(1);
         this.reelsContainer.scale.set(Math.min(Init.initialWidth * 0.5 / this.reelsContainer.width, Init.initialHeight * 0.5 / this.reelsContainer.height))
-        this.reelsContainer.position.set(Init.initialWidth / 4, window.innerHeight / 3)
+        this.reelsContainer.position.set(Init.initialWidth/4, window.innerHeight/3)
     }
 
-    addEvents() {
+    addEvents(){
         document.addEventListener('initResized', () => {
             this.resize()
         })
@@ -164,37 +160,37 @@ export class Reels {
         })
     }
 
-    changeState(newState: string) {
+    changeState(newState: string){
         this.state = newState
 
         let evt = new Event(this.state)
         document.dispatchEvent(evt)
     }
 
-    spin() {
+    spin(){
         gsap.gsap.globalTimeline.timeScale(1)
         this.spinTimeline.restart()
         this.reelsContainer.children[this.reelsContainer.children.length - 1].visible = true
     }
 
-    skip() {
+    skip(){
         console.log(this.state)
-        if (this.state == "normalState") {
+       if(this.state == "normalState"){
             gsap.gsap.globalTimeline.timeScale(100)
-        }
+       }
     }
 
-    resetArrs(self: Reels, winTimeline: gsap.TimelineMax) {
-        if (!winTimeline.isActive()) {
-            for (let i = 0; i < self.ids.length; i++) {
-                for (let j = 0; j < self.ids[i].length; j++) {
+    resetArrs(self: Reels, winTimeline: gsap.TimelineMax){
+        if(!winTimeline.isActive()){
+            for(let i = 0; i < self.ids.length; i++){
+                for(let j = 0; j < self.ids[i].length; j++){
                     (self.reelsContainer.children[i] as PIXI.Container).children[j].alpha = 1
                 }
             }
-            for (let i = 0; i < self.ids.length; i++) {
+            for(let i = 0; i < self.ids.length; i++){
                 self.ids[i] = []
             }
-            for (let key in self.idsDict) {
+            for(let key in self.idsDict){
                 self.idsDict[key] = []
             }
             self.reelsContainer.children[self.reelsContainer.children.length - 1].visible = false
@@ -202,54 +198,54 @@ export class Reels {
         }
     }
 
-    checkForWin(self: Reels) {
+    checkForWin(self: Reels){
         let time = 0
-        let winTimeline = new gsap.TimelineMax({ paused: true, autoRemoveChildren: true })
-        for (let i = 0; i < self.ids.length; i++) {
-            for (let j = 0; j < self.ids[i].length; j++) {
+        let winTimeline = new gsap.TimelineMax({paused: true, autoRemoveChildren:true})
+        for(let i = 0; i < self.ids.length; i++){
+            for(let j = 0; j < self.ids[i].length; j++){
                 (self.reelsContainer.children[i] as PIXI.Container).children[j].alpha = 0.5
             }
         }
-        for (let key in self.idsDict) {
+        for(let key in self.idsDict){
             let unq = new Set(self.idsDict[key])
-            if (unq.size > 2) {
+            if(unq.size > 2){
                 self.winAnimation(key, unq, self, time, winTimeline)
                 time += 0.7
             }
         }
-
-        if (time == 0) {
+        
+        if(time == 0){
             self.resetArrs(self, winTimeline)
         }
     }
 
-    winAnimation(val: any, cols: Set<unknown>, self: Reels, time: number, winTimeline: gsap.TimelineMax) {
+    winAnimation(val: any, cols: Set<unknown>, self: Reels, time: number, winTimeline: gsap.TimelineMax){
         let winArr = []
-        for (let id of cols) {
-            for (let i = 0; i < self.ids[id as any].length; i++) {
-                if (self.ids[id as any][i] == val) {
-                    winArr.push({ id: val, col: id, row: i });
+        for(let id of cols){
+            for(let i = 0; i < self.ids[id as any].length; i++){
+                if(self.ids[id as any][i] == val){
+                    winArr.push({id: val, col: id, row: i});
                     (self.reelsContainer.children[id as any] as PIXI.Container).children[i].alpha = 1
-                    winTimeline.to((self.reelsContainer.children[id as any] as PIXI.Container).children[i], 0.1, { x: "+=4", yoyo: true, repeat: 5 }, time);
-                    winTimeline.to((self.reelsContainer.children[id as any] as PIXI.Container).children[i], 0.1, { x: "-=4", yoyo: true, repeat: 5 }, time);
+                    winTimeline.to((self.reelsContainer.children[id as any] as PIXI.Container).children[i], 0.1, {x:"+=4", yoyo:true, repeat:5}, time);
+                    winTimeline.to((self.reelsContainer.children[id as any] as PIXI.Container).children[i], 0.1, {x:"-=4", yoyo:true, repeat:5}, time);
                 }
             }
         }
         winTimeline.play()
-        winTimeline.eventCallback("onComplete", self.resetArrs, [self, winTimeline])
-        if (val == "1") {
+        winTimeline.eventCallback("onComplete", self.resetArrs, [self, winTimeline])  
+        if(val == "1"){
             self.changeState("specialState")
         }
-
-
+        
+        
     }
 
-    addID(id: any, reelId: any) {
+    addID(id: any, reelId: any){
         this.ids[reelId].push(id)
-        if (this.idsDict[id]) {
+        if(this.idsDict[id]){
             this.idsDict[id].push(reelId)
-        }
-        else {
+        } 
+        else{
             this.idsDict[id] = [reelId]
         }
     }
